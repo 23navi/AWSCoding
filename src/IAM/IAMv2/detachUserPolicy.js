c3629fafba5e1374b8c63fed2e8fb7c2228fa77d
Navi sureka <navisureka23@gmail.com>
2023-03-19 15:07:58 +0530

    attach and detach userPolicy

---
+++ src/IAM/IAMv2/detachUserPolicy.js
@@ -0,0 +1,15 @@
+const AWS = require("aws-sdk");
+const iam = new AWS.IAM();
+
+const params={
+    PolicyArn:"arn:aws:iam::274080732168:policy/myNewCustomPolicy",
+    UserName:"New_Naviii"
+}
+
+iam.detachUserPolicy(params,(e,r)=>{
+    if(e){
+        console.log(e);
+    }else{
+        console.log(r);
+    }
+})
