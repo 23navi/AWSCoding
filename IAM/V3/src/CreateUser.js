5098abdc3bb904d6323fba46eb60ad70841df5b0
Navi sureka <navisureka23@gmail.com>
2023-03-30 21:43:39 +0530

    added some IAM V3

--- IAM/V3/src/CreateUser.js
+++ IAM/V3/src/CreateUser.js
@@ -1,3 +1,40 @@
 import {IAMClient,CreateUserCommand,GetUserCommand} from "@aws-sdk/client-iam"
 
+const iamClient= new IAMClient();
 
+const param = {
+    UserName:"Aarav",
+}
+
+
+const run = async()=>{
+    try{
+        const data = await iamClient.send(new GetUserCommand(param));
+        // if no user... it will throw an error.
+        console.log(`User with name ${param.UserName} already exist. Try other name.`)
+        return data;
+
+    }catch(e){
+        // It will run if the user already exist 
+        try{
+            const data= await iamClient.send(new CreateUserCommand(param));
+            //will throw error if aws was unable to create user...
+            console.log(`User with name ${param.UserName} was created.`)
+            return data
+
+        }catch(e){
+            console.log("New user was not created.");
+            console.log(e);
+        }
+
+    }
+}
+
+async function printResult(){
+    console.log(await run());
+}
+
+
+printResult();
+
+console.log("Straring the printResult()");
