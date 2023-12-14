c8650863327c166c456edb3e4414174ed2edca14
23navi <navisureka23@gmail.com>
2023-12-14 22:15:48 +0530

    ddbclient

--- Serverless/myWork/serverless-crud-products/ddbClient.js
+++ Serverless/myWork/serverless-crud-products/ddbClient.js
@@ -1,4 +1,5 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 const REGION = "ap-south-1";
 const ddbClient = new DynamoDBClient({ region: REGION });
+console.log("Logging from ddbClient");
 export { ddbClient };
--- Serverless/myWork/serverless-crud-products/index.js
+++ Serverless/myWork/serverless-crud-products/index.js
@@ -1,6 +1,23 @@
+import { ddbClient } from "./ddbClient";
 export const handler = async (event) => {
   console.log("Request:", JSON.stringify(event, undefined, 2));
   // TODO implement
+
+  switch (event.httpMethod) {
+    case "GET":
+      if (event.pathParameters !== null) {
+        return {
+          statusCode: 200,
+          body: JSON.stringify("Get product by id "),
+        };
+      } else {
+        return {
+          statusCode: 200,
+          body: JSON.stringify(process.env),
+        };
+      }
+  }
+
   const response = {
     statusCode: 200,
     body: JSON.stringify("Hello from Lambda!"),
