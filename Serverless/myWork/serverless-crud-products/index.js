0c879c67dde8152a1fb508eb0987e32916560fc7
23navi <navisureka23@gmail.com>
2023-12-14 22:33:23 +0530

    api shell

--- Serverless/myWork/serverless-crud-products/index.js
+++ Serverless/myWork/serverless-crud-products/index.js
@@ -6,23 +6,63 @@ export const handler = async (event) => {
   switch (event.httpMethod) {
     case "GET":
       if (event.pathParameters !== null) {
-        return {
-          statusCode: 200,
-          body: JSON.stringify("Get product by id "),
-        };
+        return getProductById(event.pathParameters.id);
       } else {
         return {
           statusCode: 200,
           body: JSON.stringify(process.env),
         };
       }
+    case "POST":
+      return createProduct(JSON.parse(event.body));
+    case "PUT":
+      return updateProductById(event.pathParameters.id, JSON.parse(event.body));
+    case "DELETE":
+      return deleteProductById(event.pathParameters.id);
+    default:
+      return {
+        statusCode: 200,
+        body: JSON.stringify("Not handled"),
+      };
   }
-
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify("Hello from Lambda!"),
-  };
-  return response;
 };
 
-handler();
+function getProductById(productId) {
+  // Find product from dynamodb
+  return {
+    statusCode: 200,
+    body: JSON.stringify(productId),
+  };
+}
+
+function getAllProducts() {
+  // get all products from dynamodb
+  return {
+    statusCode: 200,
+    body: JSON.stringify("All products"),
+  };
+}
+
+function createProduct(body) {
+  // create product in dynamodb
+  return {
+    statusCode: 200,
+    body: JSON.stringify(body),
+  };
+}
+
+function updateProductById(id, body) {
+  // update product in dynamodb
+  return {
+    statusCode: 200,
+    body: JSON.stringify(id, body),
+  };
+}
+
+function deleteProductById(id) {
+  // delete product in dynamodb
+  return {
+    statusCode: 200,
+    body: JSON.stringify(id),
+  };
+}
