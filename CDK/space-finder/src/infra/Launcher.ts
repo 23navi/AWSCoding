310eda96146a65227366aede7644b6b75e0ef4dc
23navi <navisureka23@gmail.com>
2024-10-27 11:36:10 +0530

    deploy simple version

--- CDK/space-finder/src/infra/Launcher.ts
+++ CDK/space-finder/src/infra/Launcher.ts
@@ -1,15 +1,19 @@
+import { Tags } from "aws-cdk-lib";
 import { App } from "aws-cdk-lib";
 import { ApiStack } from "./stacks/ApiStack";
 import { DataStack } from "./stacks/DataStack";
 import { LambdaStack } from "./stacks/LambdaStack";
 
-
-
 const app = new App();
-const dataStack = new DataStack(app, 'DataStack');
-const lambdaStack = new LambdaStack(app, 'LambdaStack', {
-    spacesTable: dataStack.spacesTable
-})
-new ApiStack(app, 'ApiStack', {
-    helloLambdaIntegration: lambdaStack.helloLambdaIntegration
-})
+const dataStack = new DataStack(app, "DataStack");
+const lambdaStack = new LambdaStack(app, "LambdaStack", {
+  spacesTable: dataStack.spacesTable,
+});
+const apiStack = new ApiStack(app, "ApiStack", {
+  helloLambdaIntegration: lambdaStack.helloLambdaIntegration,
+});
+
+// Add tags to group them
+Tags.of(dataStack).add("Project", "MyApp");
+Tags.of(lambdaStack).add("Project", "MyApp");
+Tags.of(apiStack).add("Project", "MyApp");
